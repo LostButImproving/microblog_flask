501055f9a70970f881d3d9bc99dc2f49411031c0
lostbutimproving <asusvenus@gmail.com>
2019-09-18 16:29:20 +0200

    added date support

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import os
 
 
@@ -19,6 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
