07dfcc9486508e1af2253894dc39d8254cb859d1
lostbutimproving <asusvenus@gmail.com>
2019-09-20 12:55:15 +0200

    added translation

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from flask import request
 from logging.handlers import SMTPHandler
 from flask import Flask
 from .config import Config
@@ -9,6 +10,7 @@ from logging.handlers import RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 import os
 
 
@@ -18,11 +20,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-from app import routes, models, errors
+babel = Babel(app)
 
 
 if not app.debug:
@@ -51,3 +53,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+from app import routes, models, errors
--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -12,5 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['asusvenus@gmail.com']
-
+    LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 25
